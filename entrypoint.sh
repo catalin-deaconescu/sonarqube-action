860875ec8cdb5ee6ee34aea98b9939d199c79426
Catalin Deaconescu <catalin.deaconescu@morningstar.com>
2023-07-09 12:08:58 +0300

    temp disable sonar for var debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,23 +23,31 @@ fi
 
 unset JAVA_HOME
 
-if [[ ! -f "${INPUT_PROJECTBASEDIR%/}sonar-project.properties" ]]; then
-  [[ -z "${INPUT_PROJECTKEY}" ]] && SONAR_PROJECTKEY="${REPOSITORY_NAME}" || SONAR_PROJECTKEY="${INPUT_PROJECTKEY}"
-  [[ -z "${INPUT_PROJECTNAME}" ]] && SONAR_PROJECTNAME="${REPOSITORY_NAME}" || SONAR_PROJECTNAME="${INPUT_PROJECTNAME}"
-  [[ -z "${INPUT_PROJECTVERSION}" ]] && SONAR_PROJECTVERSION="" || SONAR_PROJECTVERSION="${INPUT_PROJECTVERSION}"
-  sonar-scanner \
-    -Dsonar.host.url="${INPUT_HOST}" \
-    -Dsonar.projectKey="${SONAR_PROJECTKEY}" \
-    -Dsonar.projectName="${SONAR_PROJECTNAME}" \
-    -Dsonar.projectVersion="${SONAR_PROJECTVERSION}" \
-    -Dsonar.projectBaseDir="${INPUT_PROJECTBASEDIR}" \
-    -Dsonar.login="${INPUT_LOGIN}" \
-    -Dsonar.password="${SONAR_PASSWORD}" \
-    -Dsonar.sources="${INPUT_PROJECTBASEDIR}" \
-    -Dsonar.sourceEncoding="${INPUT_ENCODING}"
-else
-  sonar-scanner \
-    -Dsonar.host.url="${INPUT_HOST}" \
-    -Dsonar.login="${INPUT_LOGIN}" \
-    -Dsonar.password="${SONAR_PASSWORD}"
-fi
+echo "${INPUT_HOST}"
+echo "${INPUT_PROJECTKEY}"
+echo "${INPUT_HOST}"
+echo "${INPUT_ANGULARLOCATION}"
+echo "${INPUT_NETLOCATION}"
+
+env
+
+#if [[ ! -f "${INPUT_PROJECTBASEDIR%/}sonar-project.properties" ]]; then
+#  [[ -z "${INPUT_PROJECTKEY}" ]] && SONAR_PROJECTKEY="${REPOSITORY_NAME}" || SONAR_PROJECTKEY="${INPUT_PROJECTKEY}"
+#  [[ -z "${INPUT_PROJECTNAME}" ]] && SONAR_PROJECTNAME="${REPOSITORY_NAME}" || SONAR_PROJECTNAME="${INPUT_PROJECTNAME}"
+#  [[ -z "${INPUT_PROJECTVERSION}" ]] && SONAR_PROJECTVERSION="" || SONAR_PROJECTVERSION="${INPUT_PROJECTVERSION}"
+#  sonar-scanner \
+#    -Dsonar.host.url="${INPUT_HOST}" \
+#    -Dsonar.projectKey="${SONAR_PROJECTKEY}" \
+#    -Dsonar.projectName="${SONAR_PROJECTNAME}" \
+#    -Dsonar.projectVersion="${SONAR_PROJECTVERSION}" \
+#    -Dsonar.projectBaseDir="${INPUT_PROJECTBASEDIR}" \
+#    -Dsonar.login="${INPUT_LOGIN}" \
+#    -Dsonar.password="${SONAR_PASSWORD}" \
+#    -Dsonar.sources="${INPUT_PROJECTBASEDIR}" \
+#    -Dsonar.sourceEncoding="${INPUT_ENCODING}"
+#else
+#  sonar-scanner \
+#    -Dsonar.host.url="${INPUT_HOST}" \
+#    -Dsonar.login="${INPUT_LOGIN}" \
+#    -Dsonar.password="${SONAR_PASSWORD}"
+#fi
